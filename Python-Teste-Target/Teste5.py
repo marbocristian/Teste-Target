1cd62fd6becd1d618d1d0664dcc0c5ad97226fed
MarboCristian <54065690+marbocristian@users.noreply.github.com>
2025-03-12 06:43:04 -0300

    Delete Python-Teste-Target directory

--- Python-Teste-Target/Teste5.py
+++
@@ -1,7 +0,0 @@
-texto = input("Digite a string que você deseja inverter: ")
-texto_invertido = ""
-
-for i in range(len(texto) - 1, -1, -1):
-  texto_invertido += texto[i]
-
-print(f"String invertida: {texto_invertido}")
