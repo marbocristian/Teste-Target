7127e4d254f0947ba125d5cb813f5948964d5d65
MarboCristian <54065690+marbocristian@users.noreply.github.com>
2025-03-12 07:19:42 -0300

    Update Teste5.py

--- Python-Teste-Target/Teste5.py
+++ Python-Teste-Target/Teste5.py
@@ -1,3 +1,8 @@
+#Escreva um programa que inverta os caracteres de um string.
+
+#Essa string pode ser informada através de qualquer entrada de sua preferência ou pode ser previamente definida no código; Evite usar funções prontas, como, por exemplo, reverse;
+
+
 texto = input("Digite a string que você deseja inverter: ")
 texto_invertido = ""
 
